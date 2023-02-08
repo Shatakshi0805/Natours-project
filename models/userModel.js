5f1bcbf8f8dc226068fdf1ddc26a0602a100a589
Shatakshi <shatakshi.gupta85@gmail.com>
2023-02-08 14:37:43 +0530

    user model creation, password encryption

---
+++ models/userModel.js
@@ -0,0 +1,48 @@
+//<--------FAT MODELS, THIN CONTROLLERS! -------->
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minLength: 8
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            //ONLY WORKS ON CREATE and SAVE!!!
+            validator: function(el) {
+                return el === this.password
+            },
+            message: 'Passwords are not the same!'
+        }
+    }
+})
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 12)
+
+    this.passwordConfirm = undefined;//do not save in DB, just for verfication
+    next()
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
